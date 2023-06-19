bdc88cf1c43c5fdd00601427f812c64bb7c15af0
Luiz-H3nrique <luizsantos894@gmail.com>
2023-06-19 16:16:32 -0300

    color

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230619.161124';
+const cacheName = 'chirpy-20230619.161335';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
