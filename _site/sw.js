a14b78fd478b7e34c8b37398224d1a2eae0b1ef9
Luiz-H3nrique <luizsantos894@gmail.com>
2023-06-19 16:11:53 -0300

    teste colors

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230619.160829';
+const cacheName = 'chirpy-20230619.161124';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
